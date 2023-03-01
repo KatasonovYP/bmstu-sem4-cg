a8e6a87987bc1da10fe066256f51ed0a33872f4a
kyup21u270 <kyup21u270@bmstu.student.ru>
2023-03-02 02:59:19 +0300

    add forms to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
-import {useState} from 'react'
 import './App.css'
 import usePointStore from "./store/store";
-import Canvas from "./components/Canvas";
-import {Button, Card, Container} from "@mui/material";
+import {Container} from "@mui/material";
 import MoveForm from "./components/MoveForm";
+import RotateForm from "./components/RotateForm";
+import ScaleForm from "./components/ScaleForm";
+import Canvas from "./components/Canvas";
 
 function App() {
-	const points = usePointStore((state) => state.points);
-	const add = usePointStore(state => state.add);
-	const move = usePointStore(state => state.move);
-
 	return (
 		<div className="App">
 			<h1>Компьютерная графика Лабораторная работа №2</h1>
@@ -20,7 +17,11 @@ function App() {
 				</Container>
 
 				<Container maxWidth='xs'>
-					<MoveForm/>
+					<RotateForm/>
+				</Container>
+
+				<Container maxWidth='xs'>
+					<ScaleForm/>
 				</Container>
 			</Container>
 
