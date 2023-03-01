3be3b6e08b7f3c0e853083098e9484e00c297fe2
kyup21u270 <kyup21u270@bmstu.student.ru>
2023-03-02 02:59:06 +0300

    Add center point to canvas

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,4 +1,4 @@
-import React, {FC} from 'react';
+import React, {FC, useEffect} from 'react';
 import usePointStore from "../store/store";
 import {Circle, Layer} from "react-konva";
 import Grid from "./Grid/Grid";
@@ -6,10 +6,14 @@ import Grid from "./Grid/Grid";
 const Canvas: FC = () => {
 
 	const points = usePointStore((state) => state.points);
+	const getCenter = usePointStore((state) => state.getCenter);
 
 	return (
 		<Grid>
-			{points.map((p) => <Circle x={p.x} y={p.y} fill='#F00' radius={5}/>)}
+			{points.map((p, key) =>
+				<Circle key={key} x={p.x} y={p.y} fill='#F00' radius={5}/>
+			)}
+			<Circle x={getCenter().x} y={getCenter().y} fill='#0F0' radius={5}/>
 		</Grid>
 	);
 }
