863fb5f81a6d5c6d663e0a08a308e2342d7964ab
kyup21u270 <kyup21u270@bmstu.student.ru>
2023-03-04 22:46:57 +0300

    fix output canvas

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -9,22 +9,31 @@ const Canvas: FC = () => {
 	const edges = useFigureStore((state) => state.edges);
 	const pivot = useFigureStore((state) => state.pivot);
 
+	console.log(points)
+
 	return (
 		<Grid>
 
 			{edges.map((e, key) =>
 				<Line
 					points={[points[e[0]].x, points[e[0]].y, points[e[1]].x, points[e[1]].y]}
-					stroke='#00a'
+					stroke='#88F'
 					strokeWidth={2}
 					key={key}
 				/>)
 			}
 
-			{points.map((p, key) =>
-				<Circle key={key} x={p.x} y={p.y} fill='#F00' radius={5}/>
-			)}
-			<Circle x={pivot.x} y={pivot.y} fill='#0F0' radius={5}/>
+			{/*{points.map((p, key) =>*/}
+			{/*	<Circle*/}
+			{/*		key={key}*/}
+			{/*		x={p.x}*/}
+			{/*		y={p.y}*/}
+			{/*		fill='#44F'*/}
+			{/*		radius={3}*/}
+			{/*		onClick={e => console.log(key)}*/}
+			{/*	/>*/}
+			{/*)}*/}
+			<Circle x={pivot.x} y={pivot.y} fill='#0F0' radius={2}/>
 		</Grid>
 	);
 }
